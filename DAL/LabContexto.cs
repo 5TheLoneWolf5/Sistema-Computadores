37911941d9ecc21ab8ae279ba46be956e9a2caff
5TheLoneWolf5 <89516641+5TheLoneWolf5@users.noreply.github.com>
2024-03-11 20:58:39 -0300

    Add project files.

---
+++ DAL/LabContexto.cs
@@ -0,0 +1,18 @@
+﻿using GestaoDeLaboratorios.Controllers;
+using Microsoft.EntityFrameworkCore;
+using GestaoDeLaboratorios.Models;
+
+namespace GestaoDeLaboratorios.DAL
+{
+    public class LabContexto : DbContext
+    {
+        // public DbSet<ComputadoresController> Computadores { get; set; }
+
+        public LabContexto(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<GestaoDeLaboratorios.Models.Computadores> Computadores { get; set; } = default!;
+
+    }
+}
